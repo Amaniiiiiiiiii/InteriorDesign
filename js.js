b112728c4fc56aa16319976bd37188f7fac7300c
Amaniiiiiiiiii <118987969+Amaniiiiiiiiii@users.noreply.github.com>
2024-01-06 13:53:43 +0530

    Add files via upload

---
+++ js.js
@@ -0,0 +1,33 @@
+function toggleMenu(){
+    const toggleMenu=document.querySelector(".toggleMenu");
+    const navigation=document.querySelector(".navigation");
+    toggleMenu.classList.toggle("active");
+    navigation.classList.toggle("active");
+}
+
+//slider
+var swiper= new Swiper(".testimonial-slider",{
+    slidesPerView:1,
+    spaceBetween:20,
+     autoplay:{
+         delay:1500,
+         disableOnInteraction:false,
+     },
+    pagination:{
+        el:".swiper-pagination",
+        clickable:true,
+    },
+    breakpoints:{
+        
+    800:{
+        slidesPerView:2,
+        spaceBetween:40,
+    },
+    1200:{
+        slidesPerView:3,
+        spaceBetween:50,
+    },
+    },
+});
+//End Slider
+
